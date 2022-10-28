298b417cee72d84dd845023039b1c1cd399cfb20
Tom Bluher <tbluher14@gmail.com>
2022-10-28 11:37:01 -0600

    added styling, error handling, and persisting data to edit comment component

--- react-app/src/components/Comments/EditComment.js
+++ react-app/src/components/Comments/EditComment.js
@@ -3,45 +3,88 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, useParams } from 'react-router-dom';
 import { editCommentThunk } from "../../store/comment";
 import { getAllCommentsThunk} from "../../store/comment";
+import { getAllEventsThunk } from "../../store/event";
 
 
 const EditComment = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const sessionUser = useSelector(state => state.session.user);
     const { commentId } = useParams();
+
+    // Grabbing state variables
+    const sessionUser = useSelector(state => state.session.user);
     const specificComment = useSelector(state => state.comments[commentId])
-    const [comment, setComment] = useState('');
+    const specificEvent = useSelector(state => state.events[specificComment?.event_id])
+
+    // Specific Slice of State logic
     const eventId = specificComment?.event_id
     const userId = sessionUser.id
 
+    // Set State Variables
+    const [submitted, setSubmitted] = useState(false)
+    const [comment, setComment] = useState('');
+    const [errors, setErrors] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
 
+    // Use Effects
     useEffect((e) => {
         dispatch(getAllCommentsThunk())
-      }, [])
+        dispatch(getAllEventsThunk())
+        .then(setIsLoaded(true))
+      }, [dispatch])
+
+    // Error Handling
+    useEffect(()=> {
+        const errors = []
+        if (comment.length < 2 || comment.length  > 500) {
+            errors.push('Please enter a comment between 2 and 500 characters.')
+        }
+        setErrors(errors)
+    }, [comment])
+
+
+    // Prepopulate data
+    useEffect((e) => {
+        if (specificComment){
+            setIsLoaded(true)
+            setComment(specificComment?.comment)
+        }
+    }, [specificComment])
+
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        const commentData = {
-                user_id: userId,
-                event_id: eventId,
-                comment: comment
-            }
-
-            return dispatch(editCommentThunk(commentData, commentId))
-            .then(history.push(`/events/${specificComment?.event_id}`))
+        setSubmitted(true)
+        if (errors.length === 0){
+            const commentData = {
+                    user_id: userId,
+                    event_id: eventId,
+                    comment: comment
+                }
+                if(comment.length>2 && comment.length < 500){
+                    return dispatch(editCommentThunk(commentData, commentId))
+                    .then(history.push(`/events/${specificComment?.event_id}`))
+                }
+        }
 
         }
-        return  (
+        return  isLoaded && (
             <div className='create-comment-container'>
             <form onSubmit={handleSubmit}>
             <div className="form-container">
                 <div className="form-input-container">
+                <h3 className="add_comment_header">
+                    Edit Your Comment on {specificEvent?.name}
+                </h3>
                 </div>
                 <div className="input-container">
-                    <label htmlFor="comment" className="form-label">Edit Comment</label>
+                    <label htmlFor="comment" className="form-label"></label>
+                    <div className="comment_errors">
+                        {submitted && errors.map((error, ind) => (
+                            <div key={ind}>{error}</div>
+                        ))}
+                    </div>
                     <textarea
                     className="form-field"
                     name="comment" type="text"
