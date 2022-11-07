b1f1368879e5a26b0b639b2aebee60d38dcefd6f
Tom Bluher <tbluher14@gmail.com>
2022-11-07 08:00:12 -0700

    updates to edit event for whitespace erroring

--- react-app/src/components/Events/EditEvent.js
+++ react-app/src/components/Events/EditEvent.js
@@ -119,17 +119,18 @@ const EditEvent = () => {
             if (end_time.length < 1) errors.push('End Time for your event is required');
 
             if ((start_time<currentTime) && (start_date == currentDate())) errors.push('Please enter a Start Time in the future.')
-            if (name.length < 2 || name.length>255) errors.push('Event Name must be between 2 and 255 characters');
-            if (venue_name.length < 2 || venue_name.length > 50) errors.push('Venue Name must be between 2 and 50 Characters');
-            if (description.length < 2 || description.length>500) errors.push('Description for your event must be between 2 and 500 Characters');
-            if (address.length < 2 || address.length > 50) errors.push('Address for your event must be between 2 and 50 Characters');
-            if (city.length < 2 || city.length> 50) errors.push('City for your Event must be between 2 and 50 Characters');
-            if (state.length < 2 || state.length > 50) errors.push('State for your event must be between 2 and 50 Characters');
-            if (zip_code.length !== 5 || (!zip_code.match(zipRegX))) errors.push('Zip Code must be 5 digits');
-            if (image_url.length < 2 || !image_url.split('?')[0].match(imageRegX)) errors.push('Image Url for your event must be of type: jpg, jpeg, png, svg');
-            if (website.length < 2 || /^https:\/\//.test(website) === false && /^http:\/\//.test(website) === false){
-                 errors.push('Please enter a Website for your event (https or http)');
-                }
+            if (name.length < 2 || name.length>255 || name.includes("  ")) errors.push('Event Name must be between 2 and 255 characters without double spaces');
+            if (venue_name.length < 2 || venue_name.length > 50 || venue_name.includes("  ")) errors.push('Venue Name must be between 2 and 50 Characters without double spaces');
+            if (description.length < 2 || description.length>500 || description.includes("  ")) errors.push('Description for your event must be between 2 and 500 Characters without double spaces');
+            if (address.length < 2 || address.length > 50 || address.includes("  ")) errors.push('Address for your event must be between 2 and 50 Characters without double spaces');
+            if (city.length < 2 || city.length> 50 || city.includes("  ")) errors.push('City for your Event must be between 2 and 50 Characters without double spaces');
+            if (state.length < 2 || state.length > 50 || state.includes("  ")) errors.push('State for your event must be between 2 and 50 Characters without double spaces');
+            if (zip_code.length !== 5 || zip_code.includes("  ")|| (!zip_code.match(zipRegX))) errors.push('Zip Code must be 5 digits');
+            if (image_url.length < 1 || image_url.includes("  ") || !image_url.split('?')[0].match(imageRegX)) errors.push('Image Url for your event must be of type: jpg, jpeg, png, svg');
+            if (website.length < 1 || website.includes("  ")|| /^https:\/\//.test(website) === false && /^http:\/\//.test(website) === false){
+             errors.push('Please enter a Website for your event (https or http)');
+            }
+        ;
 
             setErrors(errors)
         }
@@ -144,15 +145,15 @@ const EditEvent = () => {
         if (errors.length === 0){
             const payload = {
                 owner_id: currentEvent.owner_id,
-                name,
-                description,
-                venue_name,
-                address,
-                city,
-                state,
-                zip_code,
-                image_url,
-                website,
+                name: name.trimStart().trimEnd(),
+                description: description.trimStart().trimEnd(),
+                venue_name: venue_name.trimStart().trimEnd(),
+                address: address.trimStart().trimEnd(),
+                city: city.trimStart().trimEnd(),
+                state: state.trimStart().trimEnd(),
+                zip_code: zip_code.trimStart().trimEnd(),
+                image_url: image_url.trimStart().trimEnd(),
+                website: website.trimStart().trimEnd(),
                 start_date,
                 start_time: start_time,
                 end_date,
