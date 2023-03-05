9832367df2093adf1efd7cc52a6841a168cb5af0
Tom Bluher <tbluher14@gmail.com>
2023-03-05 15:43:33 -0700

    updates

--- app/seeds/events.py
+++ app/seeds/events.py
@@ -3,75 +3,10 @@ from datetime import time
 
 def seed_events():
 
-    buku = Event(
-        name='WAKAAN Presents: Dual Continuum Tour w/ Buku & G-Rex',
-        venue_name='Ogden Theater',
-        description="From the track record of Robert Balotsky's work as Buku, it’s clear that the Pittsburgh native and graduate of Music Technology is a well-equipped architect of twisted, layered soundscapes. With a menacing grasp on sound design and music theory, Buku combines deceptively simple and emotive melodies, laden with resounding low-end and intricate drum patterns. With a love of drums rooted early in his childhood, his work reflects his passion for percussion laced with elements only a trained ear could produce.",
-        address='935 E Colfax Ave',
-        city='Denver',
-        state='Colorado',
-        zip_code='80218',
-        image_url='https://www.gratefulweb.com/sites/default/files/inline-images/DSC07791.jpg',
-        website='https://www.ogdentheatre.com/events/detail/448775',
-        start_date='2022-12-10',
-        start_time=time(20, 00),
-        end_date='2022-12-11',
-        end_time=time(2,00),
-        owner_id=1,
-        refunds=False
-    )
-    boombox = Event (
-        name='Boombox Featuring Backbeat Brass',
-        venue_name='Ogden Theater',
-        description="A little house, a little blues, a little funk, a little rock, and a whole lot of soul blast through BoomBox. Since first emerging in 2004, founder, songwriter, producer, and multi-instrumentalist Zion Rock Godchaux has been quietly seasoning and simmering this recipe to perfection. At the same time, the Muscle Shoals, AL native stays true to what attracted countless fans in the first place.",
-        address='935 E Colfax Ave',
-        city='Denver',
-        state='Colorado',
-        zip_code='80218',
-        image_url='https://edm.com/.image/t_share/MTUyOTU2NzQ2MDExMjU1ODE1/markusschulz-laser-lightshow.jpg',
-        website='https://www.ogdentheatre.com/events/detail/443067',
-        start_date='2022-12-31',
-        start_time=time(20, 00),
-        end_date='2023-01-01',
-        end_time=time(2,00),
-        owner_id=3,
-        refunds=False
-    )
-    event3 = Event (
-        name='dirt monkey w/ Space Wizard & Kilamanzego & Green Matter',
-        venue_name='Mission Ballroom',
-        description='Based out of Boulder, Patrick Dirt Monkey Megeath has been producing for nearly 10 years, starting with hip-hop and breaks before recently trying his hand at dubstep. With a keen ear for bass and a vast knowledge of music production, he has quickly exploded onto the scene with his innovative original productions and energetic stage presence.',
-        address='4242 Wynkoop Street',
-        city='Denver',
-        state='Colorado',
-        zip_code='80216',
-        image_url='https://static.pollstar.com/wp-content/uploads/2022/01/51209eba-448b-4bb6-9b6e-ab5ccc33604c-Float_Fest_2018_-_credit_Rick_Kern-scaled.jpg',
-        website='https://www.axs.com/artists/113296/dirt-monkey-tickets?s_kwcid=AL!7852!3!556358162040!!!g!!&cpch=SEM&cpdate=20211026&cpsrc=google&cpcn=axs-rbl-denverboulder-sem-paid&intoff=true&cpid=15086805545&utm_term=&utm_source=google&utm_medium=SEM&utm_campaign=axs-rbl-denverboulder-sem-paid&gclid=Cj0KCQjwnvOaBhDTARIsAJf8eVM9M-7VF3LVUGhUU1-H3spFllHKWqyLHAB4qGmBijipugVTocIrlAQaAk0rEALw_wcB',
-        start_date='2023-01-21',
-        start_time=time(20, 00),
-        end_date='2023-01-22',
-        end_time=time(2,00),
-        owner_id=3,
-        refunds=False
-    )
 
-    sodown = Event (
-        name='SoDown Presents Worlds Beyond Tour w/ KillBill + Jaenga',
-        venue_name='Mission Ballroom',
-        description='Ehren River Wright, aka SoDown, is a staple in the Colorado music scene. His first headline show was a huge success, selling out before doors. Thanks to a passionate fanbase, Ehren has quickly taken his music to a national stage, playing many festivals across the US. A SoDown set is not one to miss. Featuring live saxophone, Ehren takes the crowd on an intimate sensory adventure through space and time. His latest tracks include a wide array of musical talents, including his keen ear for sound design and production as well as his never ending dedication to organic instrumentation. Combining infinite influences, Ehren describes his music as an endlessly evolving medley of many different genres, culminating in a uniquely diverse style of bass music. 2018 has much promise, as SoDown continues to prove himself as one of the most dedicated and innovative acts in the game.',
-        address='4242 Wynkoop Street',
-        city='Denver',
-        state='Colorado',
-        zip_code='80216',
-        image_url='https://sodown.com/wp-content/themes/SoDown/img/fb.png?v=4',
-        website='https://sodown.com',
-        start_date='2023-02-03',
-        start_time=time(20, 00),
-        end_date='2023-02-04',
-        end_time=time(2,00),
-        owner_id=3,
-        refunds=False
-    )
+
+
+
     ofthetrees = Event(
         name='Of The Trees Presents Codex Natura w/ SuperTask',
         venue_name='Mission Ballroom',
@@ -212,11 +147,77 @@ def seed_events():
         owner_id=4,
         refunds=False
     )
+    buku = Event(
+        name='WAKAAN Presents: Dual Continuum Tour w/ Buku & G-Rex',
+        venue_name='Ogden Theater',
+        description="From the track record of Robert Balotsky's work as Buku, it’s clear that the Pittsburgh native and graduate of Music Technology is a well-equipped architect of twisted, layered soundscapes. With a menacing grasp on sound design and music theory, Buku combines deceptively simple and emotive melodies, laden with resounding low-end and intricate drum patterns. With a love of drums rooted early in his childhood, his work reflects his passion for percussion laced with elements only a trained ear could produce.",
+        address='935 E Colfax Ave',
+        city='Denver',
+        state='Colorado',
+        zip_code='80218',
+        image_url='https://www.gratefulweb.com/sites/default/files/inline-images/DSC07791.jpg',
+        website='https://www.ogdentheatre.com/events/detail/448775',
+        start_date='2023-06-10',
+        start_time=time(20, 00),
+        end_date='2022-12-11',
+        end_time=time(2,00),
+        owner_id=1,
+        refunds=False
+    )
+    boombox = Event (
+        name='Boombox Featuring Backbeat Brass',
+        venue_name='Ogden Theater',
+        description="A little house, a little blues, a little funk, a little rock, and a whole lot of soul blast through BoomBox. Since first emerging in 2004, founder, songwriter, producer, and multi-instrumentalist Zion Rock Godchaux has been quietly seasoning and simmering this recipe to perfection. At the same time, the Muscle Shoals, AL native stays true to what attracted countless fans in the first place.",
+        address='935 E Colfax Ave',
+        city='Denver',
+        state='Colorado',
+        zip_code='80218',
+        image_url='https://edm.com/.image/t_share/MTUyOTU2NzQ2MDExMjU1ODE1/markusschulz-laser-lightshow.jpg',
+        website='https://www.ogdentheatre.com/events/detail/443067',
+        start_date='2023-06-31',
+        start_time=time(20, 00),
+        end_date='2023-07-01',
+        end_time=time(2,00),
+        owner_id=3,
+        refunds=False
+    )
+
+    event3 = Event (
+        name='dirt monkey w/ Space Wizard & Kilamanzego & Green Matter',
+        venue_name='Mission Ballroom',
+        description='Based out of Boulder, Patrick Dirt Monkey Megeath has been producing for nearly 10 years, starting with hip-hop and breaks before recently trying his hand at dubstep. With a keen ear for bass and a vast knowledge of music production, he has quickly exploded onto the scene with his innovative original productions and energetic stage presence.',
+        address='4242 Wynkoop Street',
+        city='Denver',
+        state='Colorado',
+        zip_code='80216',
+        image_url='https://static.pollstar.com/wp-content/uploads/2022/01/51209eba-448b-4bb6-9b6e-ab5ccc33604c-Float_Fest_2018_-_credit_Rick_Kern-scaled.jpg',
+        website='https://www.axs.com/artists/113296/dirt-monkey-tickets?s_kwcid=AL!7852!3!556358162040!!!g!!&cpch=SEM&cpdate=20211026&cpsrc=google&cpcn=axs-rbl-denverboulder-sem-paid&intoff=true&cpid=15086805545&utm_term=&utm_source=google&utm_medium=SEM&utm_campaign=axs-rbl-denverboulder-sem-paid&gclid=Cj0KCQjwnvOaBhDTARIsAJf8eVM9M-7VF3LVUGhUU1-H3spFllHKWqyLHAB4qGmBijipugVTocIrlAQaAk0rEALw_wcB',
+        start_date='2023-07-21',
+        start_time=time(20, 00),
+        end_date='2023-07-22',
+        end_time=time(2,00),
+        owner_id=3,
+        refunds=False
+    )
+    sodown = Event (
+        name='SoDown Presents Worlds Beyond Tour w/ KillBill + Jaenga',
+        venue_name='Mission Ballroom',
+        description='Ehren River Wright, aka SoDown, is a staple in the Colorado music scene. His first headline show was a huge success, selling out before doors. Thanks to a passionate fanbase, Ehren has quickly taken his music to a national stage, playing many festivals across the US. A SoDown set is not one to miss. Featuring live saxophone, Ehren takes the crowd on an intimate sensory adventure through space and time. His latest tracks include a wide array of musical talents, including his keen ear for sound design and production as well as his never ending dedication to organic instrumentation. Combining infinite influences, Ehren describes his music as an endlessly evolving medley of many different genres, culminating in a uniquely diverse style of bass music. 2018 has much promise, as SoDown continues to prove himself as one of the most dedicated and innovative acts in the game.',
+        address='4242 Wynkoop Street',
+        city='Denver',
+        state='Colorado',
+        zip_code='80216',
+        image_url='https://sodown.com/wp-content/themes/SoDown/img/fb.png?v=4',
+        website='https://sodown.com',
+        start_date='2023-08-03',
+        start_time=time(20, 00),
+        end_date='2023-08-04',
+        end_time=time(2,00),
+        owner_id=3,
+        refunds=False
+    )
 
 
-    db.session.add(buku)
-    db.session.add(event3)
-    db.session.add(sodown)
     db.session.add(ofthetrees)
     db.session.add(wooli)
     db.session.add(event4)
@@ -225,6 +226,10 @@ def seed_events():
     db.session.add(subtronics)
     db.session.add(griz)
     db.session.add(event6)
+    db.session.add(buku)
+    db.session.add(boombox)
+    db.session.add(event3)
+    db.session.add(sodown)
 
 
 
